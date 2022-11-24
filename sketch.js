081e54b72df50d7b05d6341d65642c8ce7ca14dd
ccmsm2022B18 <118990012+ccmsm2022B18@users.noreply.github.com>
2022-11-24 12:06:08 -0300

    versão dois

--- sketch.js
+++ sketch.js
@@ -5,7 +5,13 @@ let xBol=200;
 let yBol=200;
 let diametro=50;
 let velocidadeX=1;
+let velocidadey=1
+let eux=20;
+let euy=150;
+let eul =10;
+let eua=50;
 function draw() {
+  
   background(0);
 circle(xBol, yBol, diametro);
   xBol +=velocidadeX;
@@ -16,5 +22,5 @@ circle(xBol, yBol, diametro);
   if (xBol-diametro/2<=0) {
     velocidadeX*=-1
   }
-  rect(5,150,15,100);
+  rect(20,150,10,50);
 }
